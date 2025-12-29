d07cfca4ecfded3ac8f2e213b96a0e9b1297fb34
nununuma-sabu <niigata.info.kt@gmail.com>
2025-12-29 15:55:47 +0900

    現在時刻表示対応

--- food_ticket/frontend/src/App.tsx
+++ food_ticket/frontend/src/App.tsx
@@ -1,10 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import StorePage from './pages/StorePage';
 import AdminPage from './pages/AdminPage';
+import Header from './components/Header';
 
 function App() {
   return (
     <Router>
+      <Header />
       <Routes>
         {/* 顧客向けページ */}
         <Route path="/" element={<StorePage />} />
---
+++ food_ticket/frontend/src/components/Header.tsx
@@ -0,0 +1,11 @@
+import CurrentTime from './CurrentTime';
+
+const Header = () => {
+  return (
+    <header>
+      <CurrentTime />
+    </header>
+  );
+};
+
+export default Header;
