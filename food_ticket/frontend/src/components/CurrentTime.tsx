b37cce9f6cec33fb599a8fac3d2e8a131ee581ec
nununuma-sabu <niigata.info.kt@gmail.com>
2026-01-15 09:23:53 +0900

    ヘッダーの時間表示に秒は不要だと思ったので、修正

--- food_ticket/frontend/src/components/CurrentTime.tsx
+++ food_ticket/frontend/src/components/CurrentTime.tsx
@@ -18,12 +18,11 @@ const CurrentTime = () => {
     const day = String(date.getDate()).padStart(2, '0');
     const hours = String(date.getHours()).padStart(2, '0');
     const minutes = String(date.getMinutes()).padStart(2, '0');
-    const seconds = String(date.getSeconds()).padStart(2, '0');
     
     const weekdays = ['日', '月', '火', '水', '木', '金', '土'];
     const weekday = weekdays[date.getDay()];
 
-    return `${year}年${month}月${day}日(${weekday}) ${hours}:${minutes}:${seconds}`;
+    return `${year}年${month}月${day}日(${weekday}) ${hours}:${minutes}`;
   };
 
   return (
